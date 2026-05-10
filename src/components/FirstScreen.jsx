be72dbc548ded50c46c2a2d57338f22252263ce0
krishnatinkar1aca-oss <krishnatinkar1aca@gmail.com>
2026-05-10 22:44:13 +0530

    Update text in FirstScreen component

--- src/components/FirstScreen.jsx
+++ src/components/FirstScreen.jsx
@@ -42,7 +42,7 @@ export default function FirstScreen({ onNext }) {
                     className="text-5xl md:text-6xl text-center mb-6 max-w-3xl text-balance leading-tight text-foreground font-medium"
                 >
                     I made something
-                    <span className="block text-primary">for you</span>
+                    <span className="block text-primary">for mine bby</span>
                 </motion.h1>
 
                 {/* Subtext */}
